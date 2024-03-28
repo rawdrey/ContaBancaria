61ecb07827943aa7b3fabe559d406efadef7b85d
Rawdrey <rawdrey@gmail.com>
2024-03-28 09:50:34 -0300

    construtor e sobre carga

--- contaBancaria/contaBancaria.cs
+++ contaBancaria/contaBancaria.cs
@@ -8,5 +8,20 @@ namespace contaBancaria
 {
     class contaBancaria
     {
+        private int _NumeroConta;
+        private string _NomeTitular;
+        private double _Saldo;
+        public contaBancaria(int NumeroConta, string NomeTitular)
+        {
+            _NumeroConta = _NumeroConta;
+            _NomeTitular = _NomeTitular;
+        }
+        public contaBancaria(int NumeroConta, string NomeTitular, double depositoInicial):
+            this(NumeroConta, NomeTitular)
+        {
+            depositar(depositoInicial);
+        }
+
+
     }
 }
